9f5eca3e3f20f68ab61b346b3a4c241930b9c050
PaulSklv <paulsklvv@gmail.com>
2019-08-14 21:31:44 +0200

    Updates

---
+++ precache-manifest.b3f2c9cdd95ccc5393970d72d34943d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d4866897e8f62e10eef62cae28e6c9",
+    "url": "/udemy-video-app/index.html"
+  },
+  {
+    "revision": "8944b4d3003400c72073",
+    "url": "/udemy-video-app/static/css/main.99fecfaa.chunk.css"
+  },
+  {
+    "revision": "dcea4b6da9070949e6e5",
+    "url": "/udemy-video-app/static/js/2.fdb6c12e.chunk.js"
+  },
+  {
+    "revision": "8944b4d3003400c72073",
+    "url": "/udemy-video-app/static/js/main.7e0bfdbe.chunk.js"
+  },
+  {
+    "revision": "e2830cda658e3bc9f1e8",
+    "url": "/udemy-video-app/static/js/runtime~main.290e42d2.js"
+  }
+]);
